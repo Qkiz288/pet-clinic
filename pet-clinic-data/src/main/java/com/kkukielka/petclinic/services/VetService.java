137d683a604f9848409f95e1534f1c5b3f7d84d8
Qkiz288 <qkiz288@gmail.com>
2019-07-23 11:40:14 +0200

    Created services for model. Closes #5

---
+++ pet-clinic-data/src/main/java/com/kkukielka/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package com.kkukielka.petclinic.services;
+
+import com.kkukielka.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
