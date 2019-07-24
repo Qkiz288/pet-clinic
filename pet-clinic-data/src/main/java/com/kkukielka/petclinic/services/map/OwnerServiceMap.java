e798c2b3c9c5024c547a9b87aa5c7fb7eec8fbaf
Qkiz288 <qkiz288@gmail.com>
2019-07-24 15:53:45 +0200

    closes #22

--- pet-clinic-data/src/main/java/com/kkukielka/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/kkukielka/petclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.kkukielka.petclinic.services.map;
 
 import com.kkukielka.petclinic.model.Owner;
 import com.kkukielka.petclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long>
         implements OwnerService {
 
--- pet-clinic-data/src/main/java/com/kkukielka/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/kkukielka/petclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.kkukielka.petclinic.services.map;
 
 import com.kkukielka.petclinic.model.Vet;
 import com.kkukielka.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long>
         implements VetService {
 
