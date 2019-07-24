845f55fd7fb87b9a04157d71be02236506d3e8af
Qkiz288 <qkiz288@gmail.com>
2019-07-24 16:29:08 +0200

    added Vets listing on vets/index page, closes #15

--- pet-clinic-web/src/main/java/com/kkukielka/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/kkukielka/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.kkukielka.petclinic.controllers;
 
+import com.kkukielka.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String vets() {
+    public String vets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
