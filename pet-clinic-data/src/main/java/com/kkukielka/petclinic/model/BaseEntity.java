0941f3696cfe5ea72045993e3c6b86b99b2dbb07
Qkiz288 <qkiz288@gmail.com>
2019-08-07 15:51:23 +0200

    Add project Lombok and Refactor, closes #51

--- pet-clinic-data/src/main/java/com/kkukielka/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/kkukielka/petclinic/model/BaseEntity.java
@@ -1,11 +1,17 @@
 package com.kkukielka.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
